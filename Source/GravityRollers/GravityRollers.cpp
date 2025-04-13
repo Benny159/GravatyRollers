8b85158e3415d3a0da168f5e399091486a6570f8
Benny159 <benjamin.159@web.de>
2025-04-13 20:32:00 +0200

    [ADD] basic movement

---
+++ Source/GravityRollers/GravityRollers.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GravityRollers.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GravityRollers, "GravityRollers" );
---
+++ Source/GravityRollers/Public/MarbleGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MarbleGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GRAVITYROLLERS_API AMarbleGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
